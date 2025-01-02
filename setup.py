52840226e0ffa6e7ecb7e74a39286bc9b595f625
Kevin <kevintalaue@hotmail.com>
2025-01-02 08:25:26 -0500

    updated the repo name and the files associated

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 from tele_graph import __version__
 
 setup(
-    name="tele-graph",
+    name="data-view",
     packages=find_packages(exclude=["tests"]),
     version=__version__,
     description="This open-source repository houses the code for a standalone web application designed to create interactive visualizations of telematics data.",
-    url="https://github.com/kevintalaue/tele-graph",
+    url="https://github.com/kevintalaue/data-view",
     author="kevin_talaue",
     author_email="kevintalaue@hotmail.com",
 )
